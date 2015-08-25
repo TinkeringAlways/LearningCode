7e74598aeefe821e894d44e2055bf3cc38ecbc19
TinkeringAlways <alwaystinkering@outlook.com>
2015-08-24 23:07:56 -0500

    Let's reverse a string, here's how!

---
+++ PlayingWithStringsReverse/PlayingWithStringsReverse/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayingWithStringsReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "awesome";
+
+            char[] letters = new char[word.Length];
+
+            letters = word.ToCharArray(0, word.Length);
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.WriteLine(letters[i]);
+            }
+
+            Console.ReadLine();
+
+            int cnt = 0;
+            char[] temp = new char[letters.Length];
+
+            for (int i = letters.Length - 1; i >= 0; i--)
+            {
+                temp[cnt] = letters[i];
+
+                cnt++;
+            }
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.WriteLine(temp[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
